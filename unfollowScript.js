dc020099f411bf5697789af9d1c406853dff65bf
Corey Slaven <coreyslaven@aol.com>
2017-02-08 09:58:17 -0500

    Initial commit

---
+++ unfollowScript.js
@@ -0,0 +1,6 @@
+a = setInterval(function() {
+  window.scrollTo(0, document.body.scrollHeight);
+  [].slice.call(document.querySelectorAll('.unfollow-text')).forEach(function(button) {
+    button.click();
+  });
+}, 500);
